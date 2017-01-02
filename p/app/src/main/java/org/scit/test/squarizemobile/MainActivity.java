523d0547d0e56015fb28886d800ab46153fcb1fa
hooni24 <kshmarine@gmail.com>
2017-01-02 11:04:23 +0900

    init
    init

---
+++ p/app/src/main/java/org/scit/test/squarizemobile/MainActivity.java
@@ -0,0 +1,25 @@
+package org.scit.test.squarizemobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnClick(View v){
+        switch (v.getId()){
+            case R.id.logindBtn:
+                break;
+            case R.id.registerBtn:
+                break;
+        }
+    }
+
+}
